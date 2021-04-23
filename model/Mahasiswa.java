3784b25f4f9d939cc02bcff571732b38bfc42c25
fahrianur19630387 <fahrianur32@gmail.com>
2021-04-23 23:01:10 +0800

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("============ Data Mahasiswa ============");
+  System.out.println("NPM   : 19630387");
+  System.out.println("Nama  : FAHRIANUR");
+  System.out.println("Kelas : 4A Non Reguler Banjarbaru");
+  System.out.println("========================================");
+ }
+}
